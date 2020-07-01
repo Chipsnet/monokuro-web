a1c930257121d70ac8b68cd8c92e258285f197de
Minato86 <doronomori660@outlook.jp>
2020-07-02 04:23:04 +0900

    update: リファクタリング

--- store/index.js
+++ store/index.js
@@ -1,21 +1,25 @@
 export const state = () => ({
     blogPosts: [],
-  });
-  
-  export const mutations = {
+});
+
+export const mutations = {
     setBlogPosts(state, list) {
-      state.blogPosts = list;
+        state.blogPosts = list;
     },
-  };
-  
-  export const actions = {
+};
+
+export const actions = {
     async nuxtServerInit({ commit }) {
-      let files = await require.context('~/assets/content/blog/', false, /\.json$/);
-      let blogPosts = files.keys().map(key => {
-        let res = files(key);
-        res.slug = key.slice(2, -5);
-        return res;
-      });
-      await commit('setBlogPosts', blogPosts);
+        let files = await require.context(
+            "~/assets/content/blog/",
+            false,
+            /\.json$/
+        );
+        let blogPosts = files.keys().map((key) => {
+            let res = files(key);
+            res.slug = key.slice(2, -5);
+            return res;
+        });
+        await commit("setBlogPosts", blogPosts);
     },
-  };
+};
